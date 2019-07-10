f5cb3aecb33199d4b14fd2f22b3eeca369d9c1a2
ctyiscool <taidhg.trocme1@ctyi.org>
2019-07-10 14:46:40 +0100

    addition of a timer to improve relaxation while viewing the website ass well as a gif behind it

--- timer.js
+++ timer.js
@@ -1,21 +1,47 @@
-document.getElementById("timer").innerHTML = 03 + ":" + 00;
+document.getElementById("timer").innerHTML = 01 + ":" + 00;
 document.onmousemove = resetTimer;
+Document.onmouseclick = resetTimer;
 document.onkeypress = resetTimer;
 
-function startTimer() {
-var presentTime = document.getElementById("timer").innerHTML;
-var timeArray = presentTime.split(/[:]+/);
-var m = timeArray[0];
-var s = checkSecond((timeArray[1] - 1));
-if(s==59){m=m-1}
-//if(m<0){alert('timer completed')}
+var pressCount = 0;
 
-document.getElementById("timer").innerHTML = m + ":" + s;
-setTimeout(startTimer, 1000);
+function startTimer(buttonPressed) {
+
+    if (buttonPressed == 1) {
+        pressCount = pressCount + 1;
+        //alert("First call!")
+    }
+
+    if (pressCount>1) {
+        document.getElementById("timer").innerHTML = "0:00";
+    }
+
+    var presentTime = document.getElementById("timer").innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond((timeArray[1] - 1));
+
+    //document.getElementByName("timer").disable = true;
+
+    if(s==59){m=m-1}
+    //if(m<0){alert('timer completed')}
+
+    document.getElementById("timer").innerHTML = m + ":" + s;
+
+    if ((m==0 && s==0) || m<0)  {
+        document.getElementById("timer").innerHTML = "0:00";
+        return;
+    } else {
+        setTimeout(startTimer, 1000);
+    }
 }
 
 function checkSecond(sec) {
-if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-if (sec < 0) {sec = "59"};
-return sec;
+    if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+    if (sec < 00) {sec = "59"};
+    return sec;
+}
+
+function resetTimer() {
+    document.getElementById("timer").innerHTML = "0:00";
 }
